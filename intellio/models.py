a4829d499bf0e29a4b20a969ad643cc76262d001
Nikita <iv-nikita99@mail.ru>
2024-05-30 14:44:14 +0300

    correct

--- intellio/models.py
+++ intellio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateField()
+    source = models.URLField()
+    image_url = models.URLField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
